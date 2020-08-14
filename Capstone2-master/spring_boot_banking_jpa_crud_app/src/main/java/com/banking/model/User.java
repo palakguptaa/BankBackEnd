268b5b24b769a95bf1950e83bb1000c22d8d68b9
palakguptaa <47533797+palakguptaa@users.noreply.github.com>
2020-08-14 11:02:25 +0530

    Update User.java

--- Capstone2-master/spring_boot_banking_jpa_crud_app/src/main/java/com/banking/model/User.java
+++ Capstone2-master/spring_boot_banking_jpa_crud_app/src/main/java/com/banking/model/User.java
@@ -39,6 +39,7 @@ public class User{
 	private String state;
 	private String country;
 	private String gender;
+	private String role;
 	
 	@Column(name = "email", nullable = false, unique = true)
 	private String emailId;
@@ -60,9 +61,17 @@ public class User{
 	}
 
 	
+	
+
+	
+
 	public long getuId() {
 		return uId;
 	}
+
+
+
+
 
 
 	public void setuId(long uId) {
@@ -70,41 +79,89 @@ public class User{
 	}
 
 
+
+
+
+
 	public String getFirstName() {
 		return firstName;
 	}
+
+
+
+
+
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
+
+
+
+
+
 	public String getLastName() {
 		return lastName;
 	}
+
+
+
+
+
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+
+
+
+
+
 	public long getMobileNumber() {
 		return mobileNumber;
 	}
 
-	public void setMobileNumber(long moileNumber) {
-		this.mobileNumber = moileNumber;
+
+
+
+
+
+	public void setMobileNumber(long mobileNumber) {
+		this.mobileNumber = mobileNumber;
 	}
+
+
+
+
+
 
 	public String getAddress() {
 		return address;
 	}
 
+
+
+
+
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	
+
+
+
+
+
+
 	public String getCity() {
 		return city;
 	}
+
+
+
+
 
 
 	public void setCity(String city) {
@@ -112,9 +169,17 @@ public class User{
 	}
 
 
+
+
+
+
 	public String getState() {
 		return state;
 	}
+
+
+
+
 
 
 	public void setState(String state) {
@@ -122,9 +187,17 @@ public class User{
 	}
 
 
+
+
+
+
 	public String getCountry() {
 		return country;
 	}
+
+
+
+
 
 
 	public void setCountry(String country) {
@@ -132,62 +205,143 @@ public class User{
 	}
 
 
+
+
+
+
 	public String getGender() {
 		return gender;
 	}
+
+
+
+
+
 
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
 
+
+
+
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+
+
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+
+
+
+
 	public String getEmailId() {
 		return emailId;
 	}
+
+
+
+
+
 
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
 
+
+
+
+
+
 	public String getUsername() {
 		return username;
 	}
+
+
+
+
+
 
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
+
+
+
+
+
 	public String getPassword() {
 		return password;
 	}
 
+
+
+
+
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
+
+
+
+
+
 	public boolean isEnable() {
 		return enable;
 	}
+
+
+
+
 
 
 	public void setEnable(boolean enable) {
 		this.enable = enable;
 	}
 
+
+
+
+
+
 	public SavingsAccount getSavingsAccount() {
 		return savingsAccount;
 	}
+
+
+
+
 
 
 	public void setSavingsAccount(SavingsAccount savingsAccount) {
 		this.savingsAccount = savingsAccount;
 	}
 
-	
-	
-	
+
+
+
+
+
 	public List<Requests> getRequestsList() {
 		return requestsList;
 	}
+
+
+
+
 
 
 	public void setRequestsList(List<Requests> requestsList) {
@@ -195,21 +349,29 @@ public class User{
 	}
 
 
-	
+
+
+
 
 	@Override
 	public String toString() {
 		return "User [uId=" + uId + ", firstName=" + firstName + ", lastName=" + lastName + ", mobileNumber="
-				+ mobileNumber + ", address=" + address + ", city=" + city + ", state=" + state + ", country="
-				+ country + ", gender=" + gender + ", emailId=" + emailId + ", username=" + username + ", password="
-				+ password + ", enable=" + enable + ", savingsAccount=" + savingsAccount + ", requestsList="
-				+ requestsList + "]";
+				+ mobileNumber + ", address=" + address + ", city=" + city + ", state=" + state + ", country=" + country
+				+ ", gender=" + gender + ", role=" + role + ", emailId=" + emailId + ", username=" + username
+				+ ", password=" + password + ", enable=" + enable + ", savingsAccount=" + savingsAccount
+				+ ", requestsList=" + requestsList + "]";
 	}
 
 
-	public User(String firstName, String lastName, long mobileNumber, String address, String city, String state,
-			String country, String gender, String emailId, String username, String password) {
+
+
+
+
+	public User(String firstName, String lastName, long mobileNumber, String address, String city,
+			String state, String country, String gender, String role, String emailId, String username, String password,
+			boolean enable, SavingsAccount savingsAccount, List<Requests> requestsList) {
 		super();
+	//	this.uId = uId;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.mobileNumber = mobileNumber;
@@ -218,13 +380,21 @@ public class User{
 		this.state = state;
 		this.country = country;
 		this.gender = gender;
+		this.role = role;
 		this.emailId = emailId;
 		this.username = username;
 		this.password = password;
-//		this.enable = enable;
-//		this.savingsAccount = savingsAccount;
-//		this.requestsList = requestsList;
+		this.enable = enable;
+		this.savingsAccount = savingsAccount;
+		this.requestsList = requestsList;
 	}
+
+
+
+
+
+
+
 
 
 	
